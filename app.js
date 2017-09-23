55943c5f2de17ac63eeac6f2b00c62f229cc60be
Renil <renilbabu03@gmail.com>
2017-09-24 01:15:37 +0530

    Added helmet and morgan to app.js

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-var express = require('express');
-
-var routes = require('./routes/index');
+const express 		=  require('express'),
+	  helmet  		=  require('helmet'),
+	  morgan		=  require('morgan'),
+	  bodyParser	=  require('body-parser');
 
 var app = express();
 
-var port = process.env.port || 3000;
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false })) ;
+
+//used helmet to secure headers
+app.use(helmet());
+
+// parse application/json
+app.use(bodyParser.json()) ;
+app.use(morgan('dev'));
+var routes = require('./routes/index');
+
+
+const port = process.env.port || 3000;
 
 app.set( 'view engine', 'ejs');
 
